2f8a86e499a312e6fea1d6d187f38dd2af5009ee
eugeneleychenko <nycspicebo@gmail.com>
2024-12-04 09:45:40 -0500

    Add week_4 project files

---
+++ week_4/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AnalysisWizard from "./AnalysisWizard_w4";
+
+function App() {
+  return (
+    <div className="App">
+      <AnalysisWizard />
+    </div>
+  );
+}
+
+export default App;
